cadb4447df7e521fdf4700a481eb749507d86102
JaySurj <2451shivvaran@gmail.com>
2024-05-21 16:43:08 +0200

    Files for login in

---
+++ Project-D/Project-D/ViewModels/LoginPageView.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_D.ViewModels
+{
+    public partial class LoginPageView : BaseView
+    {
+        [ObservableProperty]
+        private string _username;
+
+        [ObservableProperty]
+        private string _password;
+
+        [ICommand]
+        public async void Login()
+        {
+            // Login logic here
+        }
+    }
+}
