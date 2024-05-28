eb3786c59e503e9d7b87c76de8625f823b18eb7f
Makayru Bruney <1059964@hr.nl>
2024-05-28 17:05:32 +0200

    added Hearbeat to SQL

---
+++ Project-D/Project-D/DropboxAPIToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_D
+{
+    class DropboxAPIToken
+    {
+        public static string Token = "sl.B2CjwLY_DYCAMnqcz3WX38zlSFPoTa5JiWvNKfRg4BsB8CuZY-AdFmbCJYO_8jTZAHm-F0o3p0l0t5TI3MXnotYm5E6pCXyCjvlhCcccO7OG_NUyhB4ZoUr4csD_nms9cyBssjoijprVhDM";
+    }
+}
