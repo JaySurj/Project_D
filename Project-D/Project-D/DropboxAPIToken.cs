e12a03f819ffecf835bd412d4a54f58f4be5a93f
Valencio-Saex <valenciosaez.ict@gmail.com>
2024-05-31 21:12:27 +0200

    new token

--- Project-D/Project-D/DropboxAPIToken.cs
+++ Project-D/Project-D/DropboxAPIToken.cs
@@ -8,6 +8,6 @@ namespace Project_D
 {
     class DropboxAPIToken
     {
-        public static string Token = "sl.B2TG0XXph5nJQrRkGm_PUZxWs5SPaH0HaLPgVwmwr180m8AtcKyWIOZgPvo8KzPFsRP3Uf5OUYd31xYtVfB97dZ8fsy9yS5s9sx51NFUdJ75PyN8hhMejL48U-Vg5gBEutmwTGc5zm6xgKE";
+        public static string Token = "sl.B2R8kQFi-sPTESODAz8wnSlCvYPcbd27evNHm1C8h6iNPxq5wlIzvqeyEazIdJ-PHk8lxCkpw6dU7cv-lLUQj_u9-0RmaZsbdfgeNXN0IfSghATsdRXmQS2uDP6ASaj4t3fnBWuVRRdTGow";
     }
 }
