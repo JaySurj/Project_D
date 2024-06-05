2312ea3e81779d5a6de7383dbe484f55468b2ae8
Valencio-Saex <valenciosaez.ict@gmail.com>
2024-06-05 09:42:08 +0200

    key temporaly disabled
            public static string Token = "sl.B2mrF52XMjvMeMLgIRSQfAt4JKEZbP0uhcCQmFT51BG7n28wgKN1IrV5fv3QH8aYFDvh69B52lb9EXxnj8aKTFjWRxFtsBEIcCRbXjv7Hl7VLWFENruPIh8oQTlWeibtCk3bgIrrPGpvw6c";

--- Project-D/Project-D/DropboxAPIToken.cs
+++ Project-D/Project-D/DropboxAPIToken.cs
@@ -8,6 +8,6 @@ namespace Project_D
 {
     class DropboxAPIToken
     {
-        public static string Token = "sl.B2mrF52XMjvMeMLgIRSQfAt4JKEZbP0uhcCQmFT51BG7n28wgKN1IrV5fv3QH8aYFDvh69B52lb9EXxnj8aKTFjWRxFtsBEIcCRbXjv7Hl7VLWFENruPIh8oQTlWeibtCk3bgIrrPGpvw6c";
+        //
     }
 }
