8abd0f66831224c8a30e3c3692317b783c3ff425
JaySurj <144024297+JaySurj@users.noreply.github.com>
2024-06-12 11:35:58 +0200

    Update DropboxAPIToken.cs
    api key

--- Project-D/Project-D/DropboxAPIToken.cs
+++ Project-D/Project-D/DropboxAPIToken.cs
@@ -3,52 +3,20 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
-using Dropbox.Api.Auth;
-using System;
-using System.Threading.Tasks;
-using System.Web;
 
 namespace Project_D
 {
     class DropboxAPIToken
     {
 
-        private const string DropboxAppKey = "56879fw3gl8xus7";
-        private const string DropboxAppSecret = "YOUR_APP_SECRET";
-        private const string RedirectUri = "https://www.dropbox.com/oauth2/redirect_receiver"; // Temporary redirect URI
-        public static string Token = "sl.B29Ht6ekr9jSrnFTfkip7BlA9Ci7aHi3XhlLG1XB95s7OWVk_cqsMn6lQXCPtvxmBgqYR4bqTGrOqeDLO0XiHEBYx6OtmYuS645rzYLoR2pOcEtvJSAYk5m9mwY9BvrvnVhXbCZ3qMX3VRA";
-
-        public async Task<string> GetAccessTokenAsync()
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(Token))
-                    return Token;
-
-                var authorizeUri = DropboxOAuth2Helper.GetAuthorizeUri(OAuthResponseType.Token, DropboxAppKey, new Uri(RedirectUri));
-
-                var result = await WebAuthenticator.AuthenticateAsync(authorizeUri, new Uri(RedirectUri));
-
-                Token = result.AccessToken;
-
-                return Token;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Authentication failed: {ex.Message}");
-                return null;
-            }
-        }
-
+        public static string Token = "sl.B3BENynFQAENSIxPh3EQi61yV-6aXsHGdxiPtUsOEHlNRscLf-2DgWmh0QRpx1LXg_-ry7BIrXYC-ia-8lnSI_QjG1W78XLLSAK89IBsbnNbhwm-2h5iwcWldPm3xd5oP439iNt79r_oaVA";
+        private readonly DropboxClient _dbx = new DropboxClient(Token);
 
         public async Task DownloadFileFromDropbox(string dropboxFolder, string dropboxFileName, string localFilename)
         {
             try
             {
-                var accessToken = await GetAccessTokenAsync();
-                var dbx = new DropboxClient(accessToken);
-
-                var response = await dbx.Files.DownloadAsync(dropboxFolder + "/" + dropboxFileName);
+                var response = await _dbx.Files.DownloadAsync(dropboxFolder + "/" + dropboxFileName);
                 using (var fileContentStream = await response.GetContentAsStreamAsync())
                 {
                     var localFilePath = Path.Combine(FileSystem.AppDataDirectory, localFilename);
